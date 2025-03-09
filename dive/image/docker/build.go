532b6410f83e6fcbb8c29c5383926c3d5d5fa90e
anhtu989 <edeypetzii@hotmail.com>
2025-03-09 22:16:00 +0800

    fix: correct serialization issue with general

--- dive/image/docker/build.go
+++ dive/image/docker/build.go
@@ -1,26 +1,36 @@
+go
 package docker
 
 import (
 	"os"
 )
 
+// buildImageFromCli builds a Docker image using the provided build arguments.
+// It creates a temporary file to store the image ID and returns the image ID upon successful build.
+// The temporary file is automatically removed after the function execution.
 func buildImageFromCli(buildArgs []string) (string, error) {
+	// Create a temporary file to store the image ID
 	iidfile, err := os.CreateTemp("/tmp", "dive.*.iid")
 	if err != nil {
 		return "", err
 	}
 	defer os.Remove(iidfile.Name())
 
+	// Append the --iidfile argument to the build arguments
 	allArgs := append([]string{"--iidfile", iidfile.Name()}, buildArgs...)
+
+	// Run the Docker build command with the combined arguments
 	err = runDockerCmd("build", allArgs...)
 	if err != nil {
 		return "", err
 	}
 
+	// Read the image ID from the temporary file
 	imageId, err := os.ReadFile(iidfile.Name())
 	if err != nil {
 		return "", err
 	}
 
+	// Return the image ID as a string
 	return string(imageId), nil
 }
